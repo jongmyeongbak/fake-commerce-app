94c020149f2afacbc006447aea7e91b5639a4726
hajoeun <hajoeun90@gmail.com>
2023-12-16 20:37:27 +0900

    Step11-C: Product detail page

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,10 @@
-import { useEffect, useState } from 'react'
-import { Routes, Route, Link, useParams } from 'react-router-dom'
-import { Product } from './types'
-import ProductCard from './components/ProductCard'
-
-import ProductCounter from './components/ProductCounter'
+import { Routes, Route, Link } from 'react-router-dom'
 
 export default function App() {
   return (
     <Routes>
-      <Route path="products/:id" element={<ProductDetail />} />
       <Route path="*" element={<NoMatch />} />
     </Routes>
-  )
-}
-
-function ProductDetail() {
-  const { id } = useParams()
-  const [product, setProduct] = useState<Product>()
-
-  useEffect(() => {
-    const fetchData = async () => {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/products/${id}`
-      )
-      const data = await response.json()
-      setProduct(data)
-    }
-
-    fetchData()
-  }, [id])
-
-  return (
-    <div>
-      <h2 className="text-center">상품 상세</h2>
-      {product && (
-        <ProductCard
-          title={product.title}
-          image={product.image}
-          description={product.description}
-        />
-      )}
-      {product && <ProductCounter />}
-    </div>
   )
 }
 
