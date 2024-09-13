1f240a332745f35a30b462d229f5507a525b65c4
jongmyeongbak <116707497+jongmyeongbak@users.noreply.github.com>
2024-09-13 09:15:23 +0900

    Revert "nextjs immigration"
    This reverts commit 5bbd5089c775f4baac5586f6051511356dd7690c.

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,4 +1,4 @@
-import Link from "next/link";
+import { Link } from 'react-router-dom'
 
 const NavigationBar = () => {
   return (
@@ -6,14 +6,14 @@ const NavigationBar = () => {
       <div className="flex-1 px-2 mx-2">쇼핑</div>
       <ul className="menu menu-horizontal">
         <li>
-          <Link href="/">홈</Link>
+          <Link to="/">홈</Link>
         </li>
         <li>
-          <Link href="/products">상품 목록</Link>
+          <Link to="/products">상품 목록</Link>
         </li>
       </ul>
     </div>
-  );
-};
+  )
+}
 
-export default NavigationBar;
+export default NavigationBar
